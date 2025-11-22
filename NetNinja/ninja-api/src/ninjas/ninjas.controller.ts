69dfbc5287b7c4a10b2e3a92aff0f7cea505555d
Azimjon030201 <hazard030201@gmail.com>
2025-11-23 00:19:49 +0500

    #5 Exception Handling

--- NetNinja/ninja-api/src/ninjas/ninjas.controller.ts
+++ NetNinja/ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,41 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
-    constructor(private readonly ninjasService: NinjasService){}
+    constructor(private readonly ninjasService: NinjasService) { }
 
 
     //GET /ninjas -->[]
     @Get()
-    getNinjas(@Query("weapon") weapon:"stars"| "nunchucks"){
-        
+    getNinjas(@Query("weapon") weapon: "stars" | "nunchucks") {
+
         return this.ninjasService.getNinjas(weapon);
     }
 
     //GET /ninjas/:id --> {}
     @Get(":id")
-    getOneNinja(@Param("id") id: string){
-        return this.ninjasService.getNinja(+id);
+    getOneNinja(@Param("id") id: string) {
+        try {
+            return this.ninjasService.getNinja(+id);
+        } catch (error) {
+            throw new NotFoundException();
+        }
     }
 
     // POST /ninjas 
     @Post()
-    createNinja(@Body() createNinjaDto:CreateNinjaDto){
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
         return this.ninjasService.createNinja(createNinjaDto);
     }
 
     // PUT /ninjas/:id -->{...}
     @Put(":id")
-    updateNinja(@Param("id") id:string, @Body() updateNinja:UpdateNinjaDto){
-        return this.ninjasService.updateNinja(+id,updateNinja);
+    updateNinja(@Param("id") id: string, @Body() updateNinja: UpdateNinjaDto) {
+        return this.ninjasService.updateNinja(+id, updateNinja);
     }
 
     //DELETE /ninjas/:id
     @Delete(":id")
-    removeNinja(@Param("id") id:string){
+    removeNinja(@Param("id") id: string) {
         return this.ninjasService.removeNinja(+id);
     }
 }
